a4f203ec99dcc316709cbe76201f57f0f1bf9936
Andrew Gallagher <andrewg@andrewg.com>
2016-03-25 16:25:25 +0000

    clean up our previous mess in the manual install

--- frith-install.sh
+++ frith-install.sh
@@ -14,6 +14,11 @@ cd $PERSISTENT_VOL
 echo frith >> live-additional-software.conf
 wget -qO persistence.conf $GITHUB_ROOT/skel/persistence.conf
 
+# Before we continue, trash any stale APT config. Frith is a jealous god.
+# Also, he screwed up in the past and wants to repent.
+
+rm -rf apt/conf
+
 # ensure the peristent directories are properly created
 
 for i in apt/conf/sources.list.d apt/conf/trusted.gpg.d apt/lists apt/cache; do
@@ -39,7 +44,7 @@ chmod og= live-additional-software.conf persistence.conf
 wget -qO apt/conf/trusted.gpg.d/andrewg-codesign.gpg $GITHUB_ROOT/skel/apt/conf/trusted.gpg.d/andrewg-codesign.gpg 
 wget -qO apt/conf/sources.list.d/andrewg.list $GITHUB_ROOT/skel/apt/conf/sources.list.d/andrewg.list
 
-# now reboot to make sure everything starts up in the right place
+# reboot to make sure everything starts up in the right place
 
 echo "Rebooting in 5s to activate new configuration..."
 sleep 5
