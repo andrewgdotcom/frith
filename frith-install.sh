373c1723229eee94fb68463bd4c23b7f4c6052ec
Andrew Gallagher <andrew.gallagher@siren.io>
2018-03-04 14:08:33 +0000

    Flatten installation into one file; prereq for #27

--- frith-install.sh
+++ frith-install.sh
@@ -4,7 +4,6 @@ set -e
 TMPDIR=$(mktemp -d)
 chown amnesia $TMPDIR
 
-GITHUB_ROOT=https://github.com/andrewgdotcom/frith/raw/master
 PERSISTENT_VOL=/live/persistence/TailsData_unlocked
 
 cd $PERSISTENT_VOL
@@ -16,9 +15,13 @@ cd $PERSISTENT_VOL
 
 echo frith >> live-additional-software.conf
 
-# Drop permissions when downloading and save in a temp dir, then copy to proper location
-sudo -u amnesia wget -qO $TMPDIR/persistence.conf $GITHUB_ROOT/skel/persistence.conf
-cp $TMPDIR/persistence.conf .
+cat <<EOF > persistence.conf
+/home/amnesia/Persistent	source=Persistent
+/home/amnesia/.gnupg	source=gnupg
+/var/cache/apt/archives	source=apt/cache
+/var/lib/apt/lists	source=apt/lists
+/etc/apt/sources.list.d	source=apt/sources.list.d,link
+EOF
 
 # Before we continue, trash any stale APT config. Frith is a jealous god.
 # Also, he screwed up in the past and wants to repent.
@@ -45,15 +48,70 @@ fi
 chown tails-persistence-setup:tails-persistence-setup live-additional-software.conf persistence.conf
 chmod og= live-additional-software.conf persistence.conf
 
-# download the APT repo config directly from github
-
-# Put pubkey in sources.list.d and refer to it in the .list file
+# Put our pubkey in sources.list.d and refer to it in the .list file
 # This is safe because `apt-get update` ignores unknown file extensions
 # Don't use trusted.gpg.d as it grants global trust, which is excessive
 # https://wiki.debian.org/DebianRepository/UseThirdParty
-sudo -u amnesia wget -qO $TMPDIR/andrewg-codesign.gpg $GITHUB_ROOT/skel/apt/sources.list.d/andrewg-codesign.gpg
-sudo -u amnesia wget -qO $TMPDIR/andrewg.list $GITHUB_ROOT/skel/apt/sources.list.d/andrewg.list
-cp $TMPDIR/andrewg.list $TMPDIR/andrewg-codesign.gpg apt/sources.list.d/
+
+cat <<EOF > apt/sources.list.d/andrewg.list
+deb [signed-by=/etc/apt/sources.list.d/andrewg-codesign.gpg] tor+http://andrewg.com/debian andrewg main
+EOF
+
+cat <<EOF > $TMPDIR/andrewg-codesign.asc
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQINBFYCfLkBEACyJ2HKhzuh6TPmKLBSqI9snSjLHUxRMWX4fzyCRYF12B8pAQpQ
+jkT7Fe5yGy5mRRVdkifw0wM7ctJq/jUR0zyFnmiX8pKpF0dlGUG+Kd+ZI06NH88R
+M/gd7Mc3EG1Xzsb6FDewFMZFP/7U2aOWMntlmwNOce4O5yYwmQwqv4kgYbQyXrTg
+9V2dO50XSf7fdp2TQWKAONeqJU1ts9m3K5OExEg0lSp2mOPCwFIDVWigxvAZNCdp
+gs+/S7jx7pMdXtuG0IGOUStugulXmH/ABjD6gD/l/TuviQHrwiGW0X1Wy566b57C
+1fiEUEew9O0BZ5w7bv6euF4LdpZg2lTSKItiHTZacJCKy+hLZ+QEJy9FwLysj200
+x0KsNrH/VypYvcVwdwALCj7HmZ8OGQW+vSeHrdwZVsdApq31I+9WHNMymTruke8w
+uR8/DJ7So9ltGnf5W2Rm2wwKwQGZPf8+R9Per/lYbgYWiCA8sSr7+inV1QQI5LYV
+f8E98ywgFgXZPXUpchqcMW6D/Zj5NNb/4aiKzD5DpJlr8Nj2gwGjvFkd9OTtFzIQ
+1BOqJSj6Q1VU04xSS+gjShiJ3nnUAMCSLeXNUxqaGPh+2J6w2D5CFfMjfvWw2rae
+urAppzyOFqsjxOdwgrFvMF0kCnRACUG0tB6oU4/E6e5THbZSVItBysWWwwARAQAB
+tD5BbmRyZXcgR2FsbGFnaGVyIChjb2RlIHNpZ25pbmcga2V5KSA8YW5kcmV3Zytj
+b2RlQGFuZHJld2cuY29tPokCPgQTAQIAKAUCVgJ8uQIbAwUJA8JnAAYLCQgHAwIG
+FQgCCQoLBBYCAwECHgECF4AACgkQiOeicJL5rNwSHQ//aaMDkD1yBs6QjZKB2ll/
+4gM+/l0Bq2aXLDOVyLwyMKl8kJpO2HYwtiOaeGJOBD4Pi0SoW/OhmBoIoiJPE/pf
+PIicSzkUOaGWFms8qceUk4AQ0oeHcXkCQcvpyy9BmXeFS7mUsCWTteCk9KUU7YkO
+gM8OwQCBvPHePMVJTiwCu5cO3yxgzEhE+zthlP0wSg5Cpb4FzMEWUiyY+LHMCH83
+BgJD2WtcnQarymDtoS0ddCIMt+gwqhSdYoMDcv2S42efyzpY7DPfcmrk8U+wfpIZ
+zP8qiAXtWo/LE1f0yVXWEpHwV4rbYS+Q6N6qltvCM5VnPd2eRVhql6hakx3is0pC
+BQ+zIV1TLjSZiDThZO6ZZ5x1AWeBEONJkTbyF3Umwb+NNSI4or/ZLXeBTY3ew9pq
+fBSvTJa+klPcwBZSqKulo+ML90ie0JYmF6l4eiamJLYv8qerLt23OgQ/P85TKZba
+YH4XXKP2IFpKob74Sa9wcfW6x3NcNxTWC2AN15AyOEFB9IGB0JUIYbvxM423ozQL
+rgZrBh316zcduwcyct9IpQn2jh+DwZ9JXJb+MCTMB21MjvMzsObT6GNSQrm2rYYz
+acrPjDF9d9H6jLn4rBWKkEHO8puQl+fMLv1g4L3f1NYKNf+RBLDqMAmoElYw09oD
+wSDOZCREakOuCQHQS7q/MOKJAhwEEAEKAAYFAlYNKl8ACgkQ+3PiGvEWOTePOhAA
+lULx9+gbUjpdi4m/L7u6GhItUykqFjuLpP7sgqIeeg6W+daSSGAxkrsgBBVz8tNf
+0319zf+zwetxm4YfvLnfC9qAyDfa3j7piwRarOnNriwnPFzwAoqwQz9F3ZUtsxKK
+o5ZAVAoDeabE/l+8SXlRntQeDFw/FzYMiMICIybFJ5GNWZ6lmI7iq+D/wplFru8c
+7b+pbHuppOS1ccO6TEP1H+1Qu1BRUVMNwUgmqv0P9WVIgxcE+DqAP/y5Jr0TVqaz
+dDhtXde/QQ8qowV7o6wiMw0pWa/o5+clDlt/AftOBRN7knMFurz/tdN9JSTGy6HM
+mPbro3EU+9PXY2cTycFxZBOPb6IoodY/iDF00ofr3QJxQc0IbNPuIKq+Q/Af3o4M
+n+W/u9448tSXGozYL1VauzavKZc7figkrldpswyOxl379fDKpdaVTl2jk0Qf5KXE
+H92AcYxodY+wTTPdhEE+X+F+VL9ojVVl9XpTKEVVFsupbD2SXqnhkSjju8evp96L
+wBgLR6zYZI81GitguPZ4Z7Z1j+pPq4DZ/nA2VDRHW3hXziYYry9jXa8Bl5PbrGIJ
+Ma6hwet5Kpodl4fRZIsj6smC9LE42eGg1PKTDY3lzpPJjKiXMV67l47LGPQsNcuG
+yTeqAXcvpBhhmym0TKgx6UPKWWtj4YBV9ivojHII/0iJAj4EEwECACgCGwMGCwkI
+BwMCBhUIAgkKCwQWAgMBAh4BAheABQJYiObiBQkGSNEpAAoJEIjnonCS+azclcwQ
+AKFTVGBuc1L/i/isGezyZ3rCIsZ0kpzh/qrGIMrO9REmubHLEx362VCCxFRsacE9
+tw/9IQbR9z8oE3x48FWYkzqLxqEYfxoQGgj1tJ6R+JLMSY0dL2cYee9lwh25qZgX
+f32meclSxlqgwsp5v2bK7+DH0MwcrMr6+NijmMCsB4yAChatr9cFcbyShwuz4LbG
+Ry04r6wEgcW0AyvSVv4L0WX1HraimmtmAngy8E+G9v+tPlNK2310ta6vQ76ELgNz
+kfmBmbB9GzUewSQQENH9cxDIjnteU4o/Sui5LRr3boqHdiDt2O3Adf5Nf8p/Eoxu
+SeFkeeYURxU+44wrNPysqVWJTPYBSlp9eJ9+QBggRJRsfrs413f2D+5zfT5Zy4Fp
+eyXFvNqgWKjM9mGn9JLCiujriVTXjEu2QY2IVYYHANgRzqfh4iYDUb999Mx8kFDm
+Kf7C+RxpW6sD0KXQNUNGpkRYFEP6wr4rRc+Yo057lBMHMiPZrG6yve4kZO8TpOcB
+oRQINdqvadzdaKyKzEq/DkWY9/7wT/MuEVeNrIxnJMddUGsDKIZMYFpwbDT+aRE3
+8WcpyeP9924ziutvjvE/VDi2D3DN3E/Ttod0cXcBJDtCVxXAPMeo99q3dJ7Ss4aG
+LUKYnA5qHberqyKwKEwhod10RlDrLm1y7s6ojQX7hNhU
+=tXyi
+-----END PGP PUBLIC KEY BLOCK-----
+EOF
+gpg --no-default-keyring --keyring=apt/sources.list.d/andrewg-codesign.gpg --import $TMPDIR/andrewg-codesign.asc
 
 # reboot to make sure everything starts up in the right place
 
